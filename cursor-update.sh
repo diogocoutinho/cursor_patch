5ba1d050e5ed4f9955892f6592d7ffae47a9a0fd
Michael Stein <info@michaelstein-itb.de>
2025-06-23 16:21:37 +0200

    store useAsShipped in configfile

--- cursor-update.sh
+++ cursor-update.sh
@@ -6,33 +6,55 @@ FETCH_STABLE=$(curl --silent 'https://www.cursor.com/api/download?platform=linux
 APPIMAGE_URL=$(echo $FETCH_STABLE | jq '.downloadUrl' | tr -d '"')
 APPIMAGE_VERSION=$(echo $FETCH_STABLE | jq '.version' | tr -d '"')
 
+function writeConfig() {
+  echo "CURRENT_VERSION=$APPIMAGE_VERSION" >$HOME/.config/Cursor/LastVersion
+  echo "USE_AS_SHIPPED=$USE_AS_SHIPPED" >>$HOME/.config/Cursor/LastVersion
+}
 if [ -f $HOME/.config/Cursor/LastVersion ]; then
-    . $HOME/.config/Cursor/LastVersion
-    if [ "$CURRENT_VERSION" == "$APPIMAGE_VERSION" ]; then
-        notify-send "Updating Cursor" "Already up to date"
-        exit 0
-    fi
+  . $HOME/.config/Cursor/LastVersion
+else
+  USE_AS_SHIPPED=0
+  CURRENT_VERSION=0
+fi
+
+# Check if the first argument is --useAsShipped
+case "$1" in
+--useAsShipped)
+  USE_AS_SHIPPED=1
+  ;;
+--useAsShipped=false)
+  USE_AS_SHIPPED=0
+  ;;
+*)
+  USE_AS_SHIPPED=${USE_AS_SHIPPED:-0}
+  ;;
+esac
+
+if [ "$CURRENT_VERSION" == "$APPIMAGE_VERSION" ]; then
+  writeConfig
+  notify-send "Updating Cursor" "Already up to date"
+  exit 0
 fi
 notify-send "Updating Cursor" "Downloading latest version"
 
 mkdir -p $TEMPDIR
 pushd $TEMPDIR
 
-curl $APPIMAGE_URL --output $TEMPDIR/cursor.AppImage.original 
+curl $APPIMAGE_URL --output $TEMPDIR/cursor.AppImage.original
 chmod +x $TEMPDIR/cursor.AppImage.original
-# Check if the first argument is --useAsShipped
+
 if [ "$1" = "--useAsShipped" ]; then
-    # If --useAsShipped is provided, skip the extraction and modification
-    mkdir -p $BINDIR
-    cp $TEMPDIR/cursor.AppImage.original $BINDIR/cursor
-    chmod +x $BINDIR/cursor
-    
-    # Clean up and exit
-    popd
-    echo "CURRENT_VERSION=$APPIMAGE_VERSION" > $HOME/.config/Cursor/LastVersion
-    rm -rf $TEMPDIR
-    notify-send "Updating Cursor" "Updated to version $APPIMAGE_VERSION (as shipped)"
-    exit 0
+  # If --useAsShipped is provided, skip the extraction and modification
+  mkdir -p $BINDIR
+  cp $TEMPDIR/cursor.AppImage.original $BINDIR/cursor
+  chmod +x $BINDIR/cursor
+
+  # Clean up and exit
+  popd
+  writeConfig
+  rm -rf $TEMPDIR
+  notify-send "Updating Cursor" "Updated to version $APPIMAGE_VERSION (as shipped)"
+  exit 0
 fi
 
 # Extract the AppImage
@@ -54,6 +76,6 @@ chmod +x $BINDIR/cursor
 
 popd
 
-echo "CURRENT_VERSION=$APPIMAGE_VERSION" > $HOME/.config/Cursor/LastVersion
+writeConfig
 # Cleaning Up
 rm -rf $TEMPDIR
